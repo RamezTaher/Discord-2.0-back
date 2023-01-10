3064ddc9b8f1d5a847712c59ccb30d3eae8b5ea7
Ramez Taher <ramezgamer11@gmail.com>
2023-01-10 22:20:42 +0100

    setup typeorm & entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,24 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { entities } from './utils/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: '.env.development' }),
     AuthModule,
     UsersModule,
+    ConfigModule.forRoot({ envFilePath: '.env.development' }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB,
+      synchronize: true,
+      entities,
+    }),
   ],
   controllers: [],
   providers: [],
