98a1f561956fcd5e306daa3be20d5ab0f26a79ec
Ramez Taher <ramezgamer11@gmail.com>
2023-02-04 21:42:00 +0100

    handle messaging

--- src/gateways/gateways.ts
+++ src/gateways/gateways.ts
@@ -1,26 +1,38 @@
+import { Inject } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import {
   WebSocketGateway,
-  WebSocketServer,
   SubscribeMessage,
   MessageBody,
   OnGatewayConnection,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { Server } from 'http';
+import { Services } from 'src/utils/constants';
+import { AuthenticatedSocket } from 'src/utils/interfaces';
+import { Message } from 'src/utils/typeorm';
+import { IGatewaySessionManager } from './gateways.interfaces';
 
 @WebSocketGateway({
   cors: {
     origin: ['http://localhost:3000'],
+    credentials: true,
   },
 })
 export class MessagingGateway implements OnGatewayConnection {
-  handleConnection(client: Socket, ...args: any[]) {
-    console.log('New Incoming Connection');
-    console.log(client.id);
-    client.emit('connected', { status: 'good' });
-  }
+  constructor(
+    @Inject(Services.GATEWAYS_SESSION_MANAGER)
+    private readonly gatewaysSessionManager: IGatewaySessionManager,
+  ) {}
   @WebSocketServer()
   server: Server;
+
+  handleConnection(socket: AuthenticatedSocket, ...args: any[]) {
+    console.log('New Incoming Connection');
+    console.log(socket.user);
+    this.gatewaysSessionManager.setUserSocket(socket.user.id, socket);
+    socket.emit('connected', { status: 'good' });
+  }
 
   @SubscribeMessage('createMessage')
   handleCreateMessage(@MessageBody() data: any) {
@@ -28,9 +40,21 @@ export class MessagingGateway implements OnGatewayConnection {
   }
 
   @OnEvent('message.create')
-  handleMessageCreateEvent(payload: any) {
+  handleMessageCreateEvent(message: Message) {
     console.log('Inside message.create');
-    console.log(payload);
-    this.server.emit('onMessage', payload);
+    console.log(message);
+    const senderSocket = this.gatewaysSessionManager.getUserSocket(
+      message.sender.id,
+    );
+    const receiverSocket =
+      message.sender === message.channel.sender
+        ? this.gatewaysSessionManager.getUserSocket(message.channel.receiver.id)
+        : this.gatewaysSessionManager.getUserSocket(message.channel.sender.id);
+    if (senderSocket) {
+      senderSocket.emit('onMessage', message);
+    }
+    if (receiverSocket) {
+      receiverSocket.emit('onMessage', message);
+    }
   }
 }
